04cd989946036889231bba519be6659a749893e6
wonjoon <39115630+wnjoon@users.noreply.github.com>
2021-12-07 16:18:32 +0900

    Update network.sh

--- ethereum/poa/network.sh
+++ ethereum/poa/network.sh
@@ -30,6 +30,19 @@ else
     echo "Not Correct Command"
 fi
 
+ps -ef | grep geth | grep poa-chain-dir | grep -v grep | awk '{print $2}' | xargs kill -9
+ 
+rm -Rf ./poa-chain-dir
+ 
+geth init genesis_poa.json --datadir poa-chain-dir
+cp poaData/keystore/UTC--2021-11-25T00-45-25.293804000Z--f70e0e6ba284481deb1d53765fce4a49df5e1624 ./poa-chain-dir/keystore/
+ 
+geth --datadir=./poa-chain-dir --password ./init_accounts/password.txt account import ./init_accounts/account1
+geth --datadir=./poa-chain-dir --password ./init_accounts/password.txt account import ./init_accounts/account2
+geth --datadir=./poa-chain-dir --password ./init_accounts/password.txt account import ./init_accounts/account3
+geth --datadir=./poa-chain-dir --password ./init_accounts/password.txt account import ./init_accounts/account4
+ 
+nohup geth --networkid 13691 --datadir poa-chain-dir --nodekey ./poaData/key --http --http.port 8545 --http.api "personal,db,eth,net,web3,txpool,miner" --http.corsdomain "*" --allow-insecure-unlock --nodiscover --snapshot=false --unlock 'f70e0e6ba284481deb1d53765fce4a49df5e1624' --password init_accounts/password.txt --mine --ipcpath "/tmp/geth.ipc" > ./poa-chain-dir/gethNode.log &
 
 
 
